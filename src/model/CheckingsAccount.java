3fae22047e63dd6870e07efab2a298607e70884a
gbmira <gbiel3mira@gmail.com>
2025-10-29 19:40:43 -0300

    alterado diferenciação dos tipos de conta

--- src/model/CheckingsAccount.java
+++ src/model/CheckingsAccount.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.time.LocalDateTime;
+
 public class CheckingsAccount extends Account{
 
 	public CheckingsAccount(String accountNumber, String agencyNumber, Customer consumer, Double accountBalance,
@@ -11,8 +13,18 @@ public class CheckingsAccount extends Account{
 	public void withdraw(Double amount) {
 		if(amount < 0) throw new IllegalArgumentException("invalid withdraw");
 		
-		if (amount < getAccountBalance() && amount < getTransferLimit()) {
-			setAccountBalance(getAccountBalance() - amount);
+		int hour = LocalDateTime.now().getHour();
+		
+		double availableLimit = getTransferLimit();
+		
+		if (hour >= 22) {
+			availableLimit = 500;
+		}
+		
+		
+		double fee = 2.0;
+		if (amount + fee < getAccountBalance() && amount < availableLimit) {
+			setAccountBalance(getAccountBalance() - amount - fee);
 			addTransaction(new Transaction("SAQUE", amount, this, null));
 		} else {
 			System.out.println("Insufficient funds or Limit exceeded, try again with another value.");
--- src/model/SavingsAccount.java
+++ src/model/SavingsAccount.java
@@ -6,6 +6,13 @@ public class SavingsAccount extends Account{
 			Double transferLimit) {
 		super(accountNumber, agencyNumber, consumer, accountBalance, transferLimit);
 	}
+	
+	@Override
+	public void deposit(Double amount) {
+		super.deposit(amount);
+		double interest = amount * 0.005;
+	    setAccountBalance(getAccountBalance() + interest);
+	}
 
 	public void withdraw(Double amount) {
 		if(amount < 0) throw new IllegalArgumentException("invalid withdraw");
@@ -16,8 +23,8 @@ public class SavingsAccount extends Account{
 		} else {
 			System.out.println("Insufficient funds or Limit exceeded, try again with another value.");
 		}
-	
-	
 }
+	
+	
 }
 
